7d1986861c44f795dcad19e46f56f0cb7ef4c912
nthabi <nthabisenggladysm@gmail.com>
2022-06-05 22:19:14 +0200

    Initial Commit

---
+++ ColorMix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorMix
+//
+//  Created by IACD-014 on 2022/06/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
